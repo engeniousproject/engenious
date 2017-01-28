2958935c9582c36e832616666de2be5fa5e5de81
jvbsl <jvbsl@novacyb.com>
2017-01-28 19:01:10 +0100

    fix ffmpeg bug on windoof

--- Helper/GLSynchronizationContext.cs
+++ Helper/GLSynchronizationContext.cs
@@ -7,20 +7,51 @@ namespace engenious
 {
     sealed class GLSynchronizationContext : SynchronizationContext
     {
-        BlockingCollection<KeyValuePair<SendOrPostCallback,object>> queue = new BlockingCollection<KeyValuePair<SendOrPostCallback,object>>();
+        private Thread currentThread;
+        class CallbackState
+        {
+            public CallbackState(object state, ManualResetEvent waitHandle)
+            {
+                UserState = state;
+                WaitHandle = waitHandle;
+            }
+            public object UserState;
+            public ManualResetEvent WaitHandle;
+        }
+        BlockingCollection<KeyValuePair<SendOrPostCallback, CallbackState>> queue = new BlockingCollection<KeyValuePair<SendOrPostCallback, CallbackState>>();
 
+        public override void Send(SendOrPostCallback d, object state)
+        {
+            if (Thread.CurrentThread == currentThread)
+            {
+                d(state);
+            }
+            else
+            {
+                var waitHandle = new ManualResetEvent(false);
+                queue.Add(new KeyValuePair<SendOrPostCallback, CallbackState>(d, new CallbackState(state, waitHandle)));
+                waitHandle.WaitOne();
+                waitHandle.Dispose();
+            }
+        }
         public override void Post(SendOrPostCallback d, object state)
         {
-            queue.Add(new KeyValuePair<SendOrPostCallback,object>(d, state));
+            queue.Add(new KeyValuePair<SendOrPostCallback, CallbackState>(d, new CallbackState(state,null)));
         }
 
         public void RunOnCurrentThread()
         {
-
-            KeyValuePair<SendOrPostCallback, object> workItem;
+            currentThread = Thread.CurrentThread;
+            KeyValuePair<SendOrPostCallback, CallbackState> workItem;
 
             while (queue.Count > 0 && queue.TryTake(out workItem, Timeout.Infinite))
-                workItem.Key(workItem.Value);
+            {
+                var callbackState = workItem.Value;
+                var waitHandle = callbackState.WaitHandle;
+
+                workItem.Key(callbackState.UserState);
+                waitHandle?.Set();
+            }
 
         }
     }
