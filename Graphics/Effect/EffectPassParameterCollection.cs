3f00b4a091a4d30bbc9d1d373b7caefbb061f53f
jvbsl <jvbsl@novacyb.com>
2018-01-22 23:26:46 +0100

    fixed EffectPassParameterCollection enumeration

--- Graphics/Effect/EffectPassParameterCollection.cs
+++ Graphics/Effect/EffectPassParameterCollection.cs
@@ -7,20 +7,20 @@ namespace engenious.Graphics
 {
 	public sealed class EffectPassParameterCollection : IEnumerable<EffectPassParameter>
 	{
-		public List<EffectPassParameter> ParameterList;
+		private readonly List<EffectPassParameter> _parameterList;
 		private readonly Dictionary<string,EffectPassParameter> _parameters;
 		private readonly EffectPass _pass;
 
 		internal EffectPassParameterCollection (EffectPass pass)
 		{
 			_pass = pass;
-			ParameterList = new List<EffectPassParameter> ();
+			_parameterList = new List<EffectPassParameter> ();
 			_parameters = new Dictionary<string, EffectPassParameter> ();
 		}
 
 		internal void Add (EffectPassParameter parameter)
 		{
-			ParameterList.Add (parameter);
+			_parameterList.Add (parameter);
 			_parameters.Add (parameter.Name, parameter);
 		}
 
@@ -37,7 +37,7 @@ namespace engenious.Graphics
 
 		public EffectPassParameter this [int index] { 
 			get {
-				return ParameterList [index];
+				return _parameterList [index];
 			} 
 		}
 
@@ -52,16 +52,19 @@ namespace engenious.Graphics
 			} 
 		}
         
-        [Obsolete("Use member " + nameof(ParameterList))]
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return ParameterList.GetEnumerator();
+            return _parameterList.GetEnumerator();
         }
 
-        [Obsolete("Use member " + nameof(ParameterList))]
-        public IEnumerator<EffectPassParameter> GetEnumerator()
+        IEnumerator<EffectPassParameter> IEnumerable<EffectPassParameter>.GetEnumerator()
         {
-            return ParameterList.GetEnumerator();
+            return _parameterList.GetEnumerator();
+        }
+
+        public List<EffectPassParameter>.Enumerator GetEnumerator()
+        {
+            return _parameterList.GetEnumerator();
         }
     }
 }
