d90484b4047ec8daf77924fcb6e859609220d6c2
jvbsl <jvbsl@novacyb.com>
2018-03-20 21:50:51 +0100

    missing System.CompilerServices.Unsafe.dll was missing

--- Graphics/ConstantBuffer.cs
+++ Graphics/ConstantBuffer.cs
@@ -8,6 +8,7 @@ namespace engenious.Graphics
     public class ConstantBuffer
     {
         internal int Ubo;
+
         public ConstantBuffer(int size)
         {
             using (Execute.OnUiContext)
@@ -17,30 +18,44 @@ namespace engenious.Graphics
                 GL.BufferData(BufferTarget.UniformBuffer, new IntPtr(size), IntPtr.Zero, OpenTK.Graphics.OpenGL.BufferUsageHint.DynamicDraw);
             }
         }
-        public void Update(IntPtr data,uint size)
+
+        public unsafe void Update(IntPtr data, uint size)
         {
             using (Execute.OnUiContext)
             {
                 GL.BindBuffer(BufferTarget.UniformBuffer, Ubo);
                 var ptr = GL.MapBuffer(BufferTarget.UniformBuffer, BufferAccess.WriteOnly);
-                MemoryHelper.CopyBulk(data, ptr, size);
+
+                System.Runtime.CompilerServices.Unsafe.CopyBlock((void*) ptr, (void*) data, size);
                 //Buffer.BlockCopy(
                 GL.UnmapBuffer(BufferTarget.UniformBuffer);
             }
         }
-        public void Update<T>(T data) where T : struct
+
+        public unsafe void Update<T>(T data) where T : struct
         {
-            var size = (uint)Marshal.SizeOf(data);
-            var h = GCHandle.Alloc(data,GCHandleType.Pinned);
-            Update(h.AddrOfPinnedObject(),size);
-            h.Free();
+            using (Execute.OnUiContext)
+            {
+                GL.BindBuffer(BufferTarget.UniformBuffer, Ubo);
+                var ptr = GL.MapBuffer(BufferTarget.UniformBuffer, BufferAccess.WriteOnly);
+
+                System.Runtime.CompilerServices.Unsafe.Write((void*) ptr, data);
+
+                GL.UnmapBuffer(BufferTarget.UniformBuffer);
+            }
         }
-        public void Update<T>(T[] data) where T : struct
+
+        public unsafe void Update<T>(T[] data) where T : struct
         {
-            var size = (uint)(Marshal.SizeOf(typeof(T))*data.Length);
-            var h = GCHandle.Alloc(data,GCHandleType.Pinned);
-            Update(h.AddrOfPinnedObject(),size);
-            h.Free();
+            using (Execute.OnUiContext)
+            {
+                GL.BindBuffer(BufferTarget.UniformBuffer, Ubo);
+                var ptr = GL.MapBuffer(BufferTarget.UniformBuffer, BufferAccess.WriteOnly);
+
+                System.Runtime.CompilerServices.Unsafe.Write((void*) ptr, data);
+
+                GL.UnmapBuffer(BufferTarget.UniformBuffer);
+            }
         }
     }
 }
