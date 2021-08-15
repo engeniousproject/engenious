14b89b11f74350411988284708df8a3577d7481e
jvbsl <jvsbl@gmx.de>
2021-08-15 22:52:34 +0200

    Debugging line rendering

--- Graphics/DebugRendering.cs
+++ Graphics/DebugRendering.cs
@@ -22,7 +22,7 @@ namespace engenious.Graphics
             
             _effect = new BasicEffect(graphicsDevice);
 
-            _vertexBuffer = new VertexBuffer(graphicsDevice, VertexPosition.VertexDeclaration, 8);
+            _vertexBuffer = new VertexBuffer(graphicsDevice, VertexPosition.VertexDeclaration, 10);
             Span<VertexPosition> vertexData = stackalloc [] {
                 new VertexPosition(new Vector3(-1f, +1f, +1f)),
                 new VertexPosition(new Vector3(+1f, +1f, +1f)),
@@ -32,6 +32,8 @@ namespace engenious.Graphics
                 new VertexPosition(new Vector3(+1f, +1f, -1f)),
                 new VertexPosition(new Vector3(-1f, -1f, -1f)),
                 new VertexPosition(new Vector3(+1f, -1f, -1f)),
+                new VertexPosition(new Vector3(+0f, +0f, +0f)),
+                new VertexPosition(new Vector3(+1f, +1f, +1f)),
             };
             _vertexBuffer.SetData<VertexPosition>(vertexData);
             
@@ -67,7 +69,6 @@ namespace engenious.Graphics
         /// <param name="color">The color of the <see cref="BoundingBox"/>.</param>
         public void RenderBoundingBox(BoundingBox box, Matrix world, Matrix view, Matrix projection, Color color)
         {
-            GL.LineWidth(2);
             _graphicsDevice.VertexBuffer = _vertexBuffer;
             _graphicsDevice.IndexBuffer = _indexBuffer;
             _graphicsDevice.RasterizerState = RasterizerState.CullNone;
@@ -93,6 +94,45 @@ namespace engenious.Graphics
             }
         }
 
+
+        /// <summary>
+        /// Render a line.
+        /// </summary>
+        /// <param name="start">The start point of the line to render.</param>
+        /// <param name="end">The end point of the line to render.</param>
+        /// <param name="world">The world matrix.</param>
+        /// <param name="view">The view matrix.</param>
+        /// <param name="projection">The projection matrix.</param>
+        public void RenderLine(Vector3 start, Vector3 end, Matrix world, Matrix view, Matrix projection)
+            => RenderLine(start, end, world, view, projection, Color.White);
+
+        /// <summary>
+        /// Render a line.
+        /// </summary>
+        /// <param name="start">The start point of the line to render.</param>
+        /// <param name="end">The end point of the line to render.</param>
+        /// <param name="world">The world matrix.</param>
+        /// <param name="view">The view matrix.</param>
+        /// <param name="projection">The projection matrix.</param>
+        /// <param name="color">The color of the line to render.</param>
+        public void RenderLine(Vector3 start, Vector3 end, Matrix world, Matrix view, Matrix projection, Color color)
+        {
+            _graphicsDevice.VertexBuffer = _vertexBuffer;
+            _graphicsDevice.IndexBuffer = _indexBuffer;
+            _graphicsDevice.RasterizerState = RasterizerState.CullNone;
+            _effect.World = world * Matrix.CreateTranslation(start) * Matrix.CreateScaling(end - start);
+            _effect.View = view;
+            _effect.Projection = projection;
+            _effect.VertexColorEnabled = true;
+            _effect.TextureEnabled = false;
+            foreach (EffectPass pass in _effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                GL.VertexAttrib4(3, color.R, color.G, color.B, color.A);
+                _graphicsDevice.DrawPrimitives(PrimitiveType.Lines, 8, 2);
+            }
+        }
+
         /// <summary>
         /// Render a <see cref="BoundingFrustum"/>.
         /// </summary>
@@ -100,7 +140,19 @@ namespace engenious.Graphics
         /// <param name="world">The world matrix.</param>
         /// <param name="view">The view matrix.</param>
         /// <param name="projection">The projection matrix.</param>
-        public void RenderBoundingFrustum(BoundingFrustum frustum, Matrix world, Matrix view, Matrix projection)
+        /// <param name="color">The color of the <see cref="BoundingBox"/>.</param>
+        public void RenderBoundingFrustum(BoundingFrustum frustum, Matrix world, Matrix view, Matrix projection) =>
+            RenderBoundingFrustum(frustum, world, view, projection, Color.White);
+
+        /// <summary>
+        /// Render a <see cref="BoundingFrustum"/>.
+        /// </summary>
+        /// <param name="frustum">The <see cref="BoundingFrustum"/> to render.</param>
+        /// <param name="world">The world matrix.</param>
+        /// <param name="view">The view matrix.</param>
+        /// <param name="projection">The projection matrix.</param>
+        /// <param name="color">The color of the <see cref="BoundingFrustum"/>.</param>
+        public void RenderBoundingFrustum(BoundingFrustum frustum, Matrix world, Matrix view, Matrix projection, Color color)
         {
             var m = Matrix.Invert(frustum.Matrix);
             
@@ -121,6 +173,16 @@ namespace engenious.Graphics
                 _graphicsDevice.DrawIndexedPrimitives(PrimitiveType.Lines, 0, 0,8, 0, 8);
 
             }
+        }
+
+        /// <summary>
+        /// Gets or sets the line width for the rendering of lines.
+        /// <remarks>Not supported on every hardware; Does nothing if not supported.</remarks>
+        /// </summary>
+        public float LineWidth
+        {
+            get => GL.GetFloat(GetPName.LineWidth);
+            set => GL.LineWidth(value);
         }
     }
 }
